32e4e66e66678c4e9400c69319653aff4d85be73
ningning100613 <28201997@qq.com>
2022-05-24 01:40:49 +0800

    grngxin

--- src/pages/shop/index.jsx
+++ src/pages/shop/index.jsx
@@ -1,6 +1,9 @@
 import { Component } from 'react'
-import { View, Text, Button } from '@tarojs/components'
+import { View, Text, Button, Image } from '@tarojs/components'
 import './index.scss'
+
+import imgSrc from '../../images/img.png'
+
 
 export default class Shop extends Component {
 
@@ -16,13 +19,48 @@ export default class Shop extends Component {
 
   render() {
     return (
-      <View className='Main'>
+      <View className='Main shop'>
         <View className='fixedMain'>
-          shop
-          <View className='templates'>
-            <View className=''>qwertyuiop</View>
-            <View className='adap'>attribute</View>
+
+
+          <View className='templates shoplist'>
+            <View className='couponType'></View>
+            <View className='productPhOrder marginLeft'><Image src={imgSrc} style='width:100%;height:100%' /></View>
+            <View className='adaption productCenter'>
+              <View className='productName'>羽绒服</View>
+              <View className='templates alignItems'>
+                <View className='cost'>￥45</View>
+                <View className='adaption'></View>
+                <View className='reduceIcon'>减</View>
+                <View className='numberTip'>1</View>
+                <View className='plusIcon'>加</View>
+              </View>
+            </View>
           </View>
+
+
+          <View className='templates shoplist'>
+            <View className='couponType con'></View>
+            <View className='productPhOrder marginLeft'><Image src={imgSrc} style='width:100%;height:100%' /></View>
+            <View className='adaption productCenter'>
+              <View className='productName'>羽绒服</View>
+              <View className='templates alignItems'>
+                <View className='cost'>￥45</View>
+                <View className='adaption'></View>
+                <View className='reduceIcon'>减</View>
+                <View className='numberTip'>1</View>
+                <View className='plusIcon'>加</View>
+              </View>
+            </View>
+          </View>
+
+
+
+
+
+
+
+
 
         </View>
 
@@ -30,10 +68,14 @@ export default class Shop extends Component {
 
         <View className='fixedButtom whiteBackground padding20'>
           <View className='templates alignItems'>
+            <View className='templates alignItems'>
+              <View className='couponType'></View><Text className='font24 marginLeft'>全选</Text>
+            </View>
             <View className='adaption'></View>
-            <Button size='mini' plain className='btn'>查看订单</Button>
+            <Text className='font24'>合计：</Text>
+            <Text className='colorRed'>￥ 32.50</Text>
             <View className='marginLeft'></View>
-            <Button size='mini' className='btn-orage' type='primary' hoverClass='btn-orage-hover'>继续下单</Button>
+            <Button size='mini' className='btn-orage' type='primary' hoverClass='btn-orage-hover'>去结算</Button>
 
           </View>
         </View>
